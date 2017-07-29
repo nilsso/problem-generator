9ec55f7327b0c17bb087c2664c31775f6acfea48
Nils Olsson <nilso@enosis.net>
2017-07-29 09:56:39 -0700

    Update problem-generator.js

--- problem-generator.js
+++ problem-generator.js
@@ -130,6 +130,9 @@ function generateProblem(str) {
 // Render a new problem
 function renderNewProblem() {
   problem.fadeOut(300, function() {
+    console.debug(
+      generator_menu.val(),
+      generators[generator_menu.val()])
     problem.html(generateProblem(generators[generator_menu.val()]));
     MathJax.Hub.Queue(["Typeset", MathJax.Hub], function() {
       problem.fadeIn(300);
