1106dbc35f7fb5694655f280f54ba9f9a41701d6
Nils Olsson <nilso@enosis.net>
2017-07-28 18:30:10 -0700

    Update

--- problem_generator.js
+++ problem_generator.js
@@ -1,26 +1,8 @@
-// -----------------------------------------------------------------------------
-// API
 
 // Generate random integer
 function randomInt(min, max) {
   return Math.floor(Math.random()*(max-min+1))+min;
 }
-
-// Generate random problem from generator string
-function generateProblem(str) {
-  var symbols = [];
-  str.replace(/\%\w/g,
-    match => {
-      if (symbols.indexOf(match) == -1) symbols.push(match)});
-  for (i in symbols) {
-    var value = randomInt(1, 10);
-    str = str.replace(RegExp(symbols[i], 'g'), value);
-  }
-  return str;
-}
-
-// -----------------------------------------------------------------------------
-// Content
 
 // Elements
 var problem = $("#problem");
@@ -28,14 +10,39 @@ var generator_menu = $("#generators");
 
 // Generator strings
 generators = {
-  "Geometric sequence" : "it's nothing!",
-  "Geometric series"   : "\\[\\sum_{n=1}^\\infty %a(%r)^n\\]"
+  "Arithmetic sequence" : function() {
+    var a = [ randomInt(-5, 10) ];
+    var d = randomInt(1,5);
+    for (i = 0; i < 4; ++i) { a.push(a[0]+(i+1)*d); }
+    var s = "\\[\\{ " + a.join(",") + ",...\\}\\]";
+    return s;
+  },
+  "Geometric sequence" : function() {
+    var a = [0];
+    while (a[0] == 0) { a[0] = randomInt(-5, 10); }
+    var r = randomInt(2,10);
+    for (i = 1; i < 5; ++i) { a.push(a[0]*Math.pow(r, i)); }
+    var s = "\\[\\{ " + a.join(",") + ",...\\}\\]";
+    return s;
+  },
+  "Geometric series" : function() {
+    var s = "\\[\\sum_{n=1}^\\infty %a(%r)^n\\]";
+    var symbols = [];
+    s.replace(/\%\w/g,
+      match => {
+        if (symbols.indexOf(match) == -1) symbols.push(match)});
+    for (i in symbols) {
+      var value = randomInt(1, 10);
+      s = s.replace(RegExp(symbols[i], 'g'), value);
+    }
+    return s;
+  }
 }
 
 // Populate generator menu
 $.each(generators, function(key, value) {
   generator_menu.append(
-    $("<option></option>").val(value).html(key)
+    $("<option></option>").html(key)
   );
 });
 
@@ -45,7 +52,7 @@ generator_menu.val($.jStorage.get("generator", generator_menu.val()));
 // Render a new problem
 function renderNewProblem() {
   problem.fadeOut(300, function() {
-    problem.html(generateProblem(generator_menu.val()))
+    problem.html(generators[generator_menu.val()]());
     MathJax.Hub.Queue(["Typeset", MathJax.Hub], function() {
       problem.fadeIn(300);
     });
