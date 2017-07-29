cd0d176120563c1bc0731c2ff6d8c795db3a9b48
Nils Olsson <nilso@enosis.net>
2017-07-28 22:44:34 -0700

    Update

--- problem_generator.js
+++ problem_generator.js
@@ -10,33 +10,9 @@ var generator_menu = $("#generators");
 
 // Generator strings
 generators = {
-  "Arithmetic sequence" : function() {
-    var a = [ randomInt(-5, 10) ];
-    var d = randomInt(1,5);
-    for (i = 0; i < 4; ++i) { a.push(a[0]+(i+1)*d); }
-    var s = "\\[\\{ " + a.join(",") + ",...\\}\\]";
-    return s;
-  },
-  "Geometric sequence" : function() {
-    var a = [0];
-    while (a[0] == 0) { a[0] = randomInt(-5, 10); }
-    var r = randomInt(2,10);
-    for (i = 1; i < 5; ++i) { a.push(a[0]*Math.pow(r, i)); }
-    var s = "\\[\\{ " + a.join(",") + ",...\\}\\]";
-    return s;
-  },
-  "Geometric series" : function() {
-    var s = "\\[\\sum_{n=1}^\\infty %a(%r)^n\\]";
-    var symbols = [];
-    s.replace(/\%\w/g,
-      match => {
-        if (symbols.indexOf(match) == -1) symbols.push(match)});
-    for (i in symbols) {
-      var value = randomInt(1, 10);
-      s = s.replace(RegExp(symbols[i], 'g'), value);
-    }
-    return s;
-  }
+  "Arithmetic sequence": "\\[\\{%{a0},%{a1},%{a2},%{a3},%{a4},...\\}\\]%{a:arithmetic}",
+  "Geometric sequence": "\\[\\{%{a0},%{a1},%{a2},%{a3},%{a4},...\\}\\]%{a:geometric}",
+  "Geometric series": "\\[\\sum_{n=1}^\\infty %{a}(%{r})^n\\]"
 }
 
 // Populate generator menu
@@ -49,10 +25,95 @@ $.each(generators, function(key, value) {
 // Set default generator (or from browser cookie)
 generator_menu.val($.jStorage.get("generator", generator_menu.val()));
 
+// The meat; the problem generator
+function generateProblem(str) {
+  // TODO: Implement custom number generators contained within a table
+  // e.g. num_gen["arithmetic"]
+  //      num_gen["geom"]
+  // And each with custom arguments such as alternating, fractions, etc.
+  //
+  // Utilization will entail replacing the switch statement for handling symbol
+  // behavior, initial value generation for single symbols and array.
+  //
+  var symbols = {};
+  // Compile symbol table
+  str.replace(/%{((\w)(\d*))}/g, function(f, m, s, i) {
+    // f : fully matched token
+    // m : symbol and index
+    // s : just the symbol (e.g. the a of "a", the b of "b3")
+    // i : just the index (e.g. the 1 of "a1", the 0 of "b0")
+    if (symbols[s] == undefined) {
+      // New symbol
+      if (i) symbols[s] = [ randomInt(1,10) ];
+      else symbols[s] = randomInt(1,10);
+    } else {
+      // Existing symbol
+      if (i) {
+        if (typeof(symbols[s]) != "object") {
+          throw('ProbGen error: symbol "' + m + '" previously specified as number');
+        } else if (symbols[s].length > i) {
+          throw('ProbGen error: index "' + i + '" greater than "' + s + '" length (' + symbols[s].length + ')');
+        }
+        ++symbols[s].length;
+      } else if (typeof(symbols[s]) != "number") {
+        throw('ProbGen error: symbol "' + m + '" previously specified as array');
+      }
+    }
+  });
+  // Handle symbol behavior
+  str.replace(/%{(\w):\w*}/g, function(_, s) {
+    if (symbols[s] == undefined)
+      throw('ProbGen error: generator rule defined for non-existant symbol "'+s+'"');
+  });
+  for (s in symbols) {
+    if (typeof(symbols[s]) == "object") {
+      var e = RegExp("%{"+s+":(\\w+)}");
+      var m = str.match(RegExp(e, "g"));
+      if ((m && m.length) != 1) {
+        throw('ProbGen error: expected 1 generator rule for symbol "'+s+'" but got '+ ((m && m.length) || 0));
+      }
+      var g = str.match(e)[1];
+      switch (g) {
+        case "arithmetic":
+          var a = randomInt(-10, 10);
+          var d = randomInt(1, 10);
+          for (i = 0; i < symbols[s].length; ++i) {
+            symbols[s][i] = a+d*i;
+          }
+          break;
+        case "geometric":
+          var a = randomInt(-10, 10);
+          var r = randomInt(1, 10);
+          for (i = 0; i < symbols[s].length; ++i) {
+            symbols[s][i] = a*Math.pow(r,i);
+          }
+          break;
+        default:
+          throw('ProbGen error: unrecognized generator "'+g+'"');
+          break;
+      }
+    }
+    str = str.replace(e, "");
+  }
+  // Generate LaTeX string
+  for (k in symbols) {
+    if (typeof(symbols[k]) == "number") {
+      var e = RegExp("%{"+k+"}", "g");
+      str = str.replace(e, symbols[k]);
+    } else {
+      for (i = 0; i < symbols[k].length; ++i) {
+        var e = RegExp("%{"+k+i+"}", "g");
+        str = str.replace(e, symbols[k][i]);
+      }
+    }
+  }
+  return str;
+}
+
 // Render a new problem
 function renderNewProblem() {
   problem.fadeOut(300, function() {
-    problem.html(generators[generator_menu.val()]());
+    problem.html(generateProblem(generators[generator_menu.val()]));
     MathJax.Hub.Queue(["Typeset", MathJax.Hub], function() {
       problem.fadeIn(300);
     });
